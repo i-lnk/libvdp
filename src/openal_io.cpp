62903eca2300e829323dfcc7ecefe757b50a0824
i-lnk <zzj@i-lnk.com>
2016-12-19 17:44:36 +0800

    add openal_io for iOS audio play and capture.

---
+++ src/openal_io.cpp
@@ -0,0 +1,9 @@
+//
+//  openal_io.cpp
+//  libvdp
+//
+//  Created by fork on 16/12/19.
+//  Copyright © 2016年 fork. All rights reserved.
+//
+
+#include "openal_io.hpp"
---
+++ src/openal_io.h
@@ -0,0 +1,14 @@
+//
+//  openal_io.hpp
+//  libvdp
+//
+//  Created by fork on 16/12/19.
+//  Copyright © 2016年 fork. All rights reserved.
+//
+
+#ifndef openal_io_hpp
+#define openal_io_hpp
+
+#include <stdio.h>
+
+#endif /* openal_io_hpp */
